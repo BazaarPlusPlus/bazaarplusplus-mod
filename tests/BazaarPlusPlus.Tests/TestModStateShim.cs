6b799c4b64d3792d9246c6413ec1cff4ec8644ba
Xinyu Yang <8@cau.edu.cn>
2026-03-12 20:36:05 +0800

    refactor: remove obsolete test files and project configuration for BazaarPlusPlus.Tests

--- tests/BazaarPlusPlus.Tests/TestModStateShim.cs
+++
@@ -1,9 +0,0 @@
-using BepInEx.Logging;
-
-namespace BazaarPlusPlus;
-
-internal static class ModState
-{
-    public static ManualLogSource? Logger { get; set; }
-    public static string? CardsJsonPath { get; set; }
-}
