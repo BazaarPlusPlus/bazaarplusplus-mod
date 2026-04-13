bd79db1e90e76a5acd20e1eff485c7a2387f23f1
Xinyu Yang <8@cau.edu.cn>
2026-04-13 22:08:45 +0800

    Clarify plugin startup flow

--- Plugin.cs
+++ Plugin.cs
@@ -2,13 +2,13 @@
 #nullable enable
 using System;
 using System.IO;
+using BazaarPlusPlus.Core.Config;
 using BazaarPlusPlus.Core.Runtime;
 using BazaarPlusPlus.Game.CombatReplay;
 using BazaarPlusPlus.Game.CombatStatusBar;
 using BazaarPlusPlus.Game.HistoryPanel;
 using BazaarPlusPlus.Game.Identity;
 using BazaarPlusPlus.Game.MonsterPreview;
-using BazaarPlusPlus.Game.RunLifecycle;
 using BazaarPlusPlus.Game.RunLogging;
 using BazaarPlusPlus.Game.RunLogging.Upload;
 using BazaarPlusPlus.Game.Screenshots;
@@ -16,6 +16,7 @@ using BazaarPlusPlus.Game.Tooltips;
 using BepInEx;
 using BepInEx.Configuration;
 using HarmonyLib;
+using UnityEngine;
 
 namespace BazaarPlusPlus;
 
@@ -24,54 +25,47 @@ public class Plugin : BaseUnityPlugin
 {
     private readonly Harmony _harmony = new Harmony(MyPluginInfo.PLUGIN_GUID);
     private BppRuntimeHost? _runtimeHost;
+    private bool _patchesApplied;
 
     protected virtual void Awake()
     {
         try
         {
-            BppPluginVersion.Initialize(Info.Location);
-            var configFile = CreateConfigFile();
-            var runtime = InstallRuntimeHost(configFile);
-            var services = runtime.Services;
-            var combatReplayRuntime = runtime.CombatReplayRuntime;
             BppLog.Info("Plugin", $"Plugin {MyPluginInfo.PLUGIN_GUID} loaded");
+            BppPluginVersion.Initialize(Info.Location);
 
-            BppLog.Info("Plugin", "Applying Harmony patches");
-            _harmony.PatchAll();
-            BppLog.Info("Plugin", "Harmony patches applied");
+            var configFile = CreatePluginConfigFile();
+            var runtime = CreateAndStartRuntime(configFile);
 
-            BppLog.Info("Plugin", "Attaching runtime components");
-            AttachRuntimeComponents(services, () => combatReplayRuntime);
-            BppLog.Info("Plugin", "Runtime components attached");
-
-            BppLog.Info("Plugin", "Plugin components attached");
+            ApplyHarmonyPatches();
+            AttachRuntimeComponents(runtime.Services, runtime.CombatReplayRuntime);
+            BppLog.Info("Plugin", "Plugin initialization completed");
         }
         catch (Exception ex)
         {
             BppLog.Error("Plugin", "Plugin initialization failed", ex);
+            CleanupFailedInitialization();
             throw;
         }
     }
 
     protected virtual void OnDestroy()
     {
-        _harmony.UnpatchSelf();
-        _runtimeHost?.Stop();
+        StopRuntimeHost();
+        UnpatchHarmony();
         BppLog.Flush();
     }
 
-    private ConfigFile CreateConfigFile()
+    private ConfigFile CreatePluginConfigFile()
     {
         var configFile = new ConfigFile(Path.Combine(Paths.ConfigPath, "BazaarPlusPlus.cfg"), true);
         HistoryPanelPreviewSettings.Initialize(configFile);
         return configFile;
     }
 
-    private (
-        BppRuntimeServices Services,
-        RunLifecycleModule LifecycleModule,
-        CombatReplayRuntime CombatReplayRuntime
-    ) InstallRuntimeHost(ConfigFile configFile)
+    private (BppRuntimeServices Services, CombatReplayRuntime CombatReplayRuntime) CreateAndStartRuntime(
+        ConfigFile configFile
+    )
     {
         CombatReplayRuntime? combatReplayRuntime = null;
         _runtimeHost = new BppRuntimeHost(
@@ -80,47 +74,114 @@ public class Plugin : BaseUnityPlugin
             configFile,
             () => combatReplayRuntime
         );
+
+        BppLog.Info("Plugin", "Installing runtime host");
         _runtimeHost.Install();
+
+        BppLog.Info("Plugin", "Adding CombatReplayRuntime");
         combatReplayRuntime = gameObject.AddComponent<CombatReplayRuntime>();
+
+        BppLog.Info("Plugin", "Starting runtime host");
         _runtimeHost.Start();
-        return (_runtimeHost.Services, _runtimeHost.LifecycleModule, combatReplayRuntime);
+
+        return (_runtimeHost.Services, combatReplayRuntime);
+    }
+
+    private void ApplyHarmonyPatches()
+    {
+        BppLog.Info("Plugin", "Applying Harmony patches");
+        _harmony.PatchAll();
+        _patchesApplied = true;
+        BppLog.Info("Plugin", "Harmony patches applied");
     }
 
     private void AttachRuntimeComponents(
         BppRuntimeServices services,
-        Func<CombatReplayRuntime?> combatReplayRuntimeAccessor
+        CombatReplayRuntime combatReplayRuntime
     )
     {
-        BppLog.Info("Plugin", "Adding RunLoggingController");
+        BppLog.Info("Plugin", "Attaching runtime components");
         gameObject.AddComponent<RunLoggingController>();
-        BppLog.Info("Plugin", "Adding RunUploadController");
         gameObject.AddComponent<RunUploadController>();
-        BppLog.Info("Plugin", "Adding PlayerObservationController");
         gameObject.AddComponent<PlayerObservationController>();
+        AddConfiguredHistoryPanel(services, combatReplayRuntime);
+        gameObject.AddComponent<CombatStatusBar>();
+        gameObject.AddComponent<MonsterPreviewWarmupController>();
+        gameObject.AddComponent<CardSetPreviewRuntime>();
+        gameObject.AddComponent<MonsterPreviewItemBoardRuntime>();
+        gameObject.AddComponent<EndOfRunScreenshotController>();
+        AddConfiguredTooltipModifierRefreshController(services.Config);
+        BppLog.Info("Plugin", "Runtime components attached");
+    }
+
+    private void AddConfiguredHistoryPanel(
+        BppRuntimeServices services,
+        CombatReplayRuntime combatReplayRuntime
+    )
+    {
         BppLog.Info("Plugin", "Adding HistoryPanel");
         var historyPanel = gameObject.AddComponent<HistoryPanel>();
+
         var historyPanelRuntime = new HistoryPanelRuntime(
             services.RunContext,
             services.Paths.RunLogDatabasePath,
             services.Paths.CombatReplayDirectoryPath,
-            combatReplayRuntimeAccessor
+            () => combatReplayRuntime
         );
         historyPanel.Configure(HistoryPanelFactory.Create(historyPanelRuntime));
-        BppLog.Info("Plugin", "Adding CombatStatusBar");
-        gameObject.AddComponent<CombatStatusBar>();
-        BppLog.Info("Plugin", "Adding MonsterPreviewWarmupController");
-        gameObject.AddComponent<MonsterPreviewWarmupController>();
-        BppLog.Info("Plugin", "Adding CardSetPreviewRuntime");
-        gameObject.AddComponent<CardSetPreviewRuntime>();
-        BppLog.Info("Plugin", "Adding MonsterPreviewItemBoardRuntime");
-        gameObject.AddComponent<MonsterPreviewItemBoardRuntime>();
-        BppLog.Info("Plugin", "Adding EndOfRunScreenshotController");
-        gameObject.AddComponent<EndOfRunScreenshotController>();
+    }
 
+    private void AddConfiguredTooltipModifierRefreshController(IBppConfig config)
+    {
         BppLog.Info("Plugin", "Adding TooltipModifierRefreshController");
         var tooltipModifierRefreshController =
             gameObject.AddComponent<TooltipModifierRefreshController>();
-        tooltipModifierRefreshController.Initialize(services.Config);
+        tooltipModifierRefreshController.Initialize(config);
         BppLog.Info("Plugin", "TooltipModifierRefreshController initialized");
+    }
+
+    private void CleanupFailedInitialization()
+    {
+        DetachRuntimeComponents();
+        StopRuntimeHost();
+        UnpatchHarmony();
+    }
+
+    private void StopRuntimeHost()
+    {
+        _runtimeHost?.Stop();
+        _runtimeHost = null;
+    }
+
+    private void UnpatchHarmony()
+    {
+        if (!_patchesApplied)
+            return;
+
+        _harmony.UnpatchSelf();
+        _patchesApplied = false;
+    }
+
+    private void DetachRuntimeComponents()
+    {
+        DestroyComponentIfPresent<TooltipModifierRefreshController>();
+        DestroyComponentIfPresent<EndOfRunScreenshotController>();
+        DestroyComponentIfPresent<MonsterPreviewItemBoardRuntime>();
+        DestroyComponentIfPresent<CardSetPreviewRuntime>();
+        DestroyComponentIfPresent<MonsterPreviewWarmupController>();
+        DestroyComponentIfPresent<CombatStatusBar>();
+        DestroyComponentIfPresent<HistoryPanel>();
+        DestroyComponentIfPresent<PlayerObservationController>();
+        DestroyComponentIfPresent<RunUploadController>();
+        DestroyComponentIfPresent<RunLoggingController>();
+        DestroyComponentIfPresent<CombatReplayRuntime>();
+    }
+
+    private void DestroyComponentIfPresent<T>()
+        where T : Component
+    {
+        var component = GetComponent<T>();
+        if (component != null)
+            UnityEngine.Object.DestroyImmediate(component);
     }
 }
