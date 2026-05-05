5405a3dd0845dc470cf4f28fcf2c72b04fc5e40a
Xinyu Yang <8@cau.edu.cn>
2026-05-06 04:42:39 +0800

    Drop ShouldReRegister from V3 HTTP failure classifier

--- Game/Online/V3HttpFailureClassifier.cs
+++ Game/Online/V3HttpFailureClassifier.cs
@@ -3,21 +3,16 @@ namespace BazaarPlusPlus.Game.Online;
 
 internal readonly struct V3HttpFailureDecision
 {
-    public V3HttpFailureDecision(bool shouldFallback, bool shouldReRegister)
+    public V3HttpFailureDecision(bool shouldFallback)
     {
         ShouldFallback = shouldFallback;
-        ShouldReRegister = shouldReRegister;
     }
 
     public bool ShouldFallback { get; }
-    public bool ShouldReRegister { get; }
 }
 
 internal static class V3HttpFailureClassifier
 {
     public static V3HttpFailureDecision Classify(int statusCode) =>
-        new(
-            shouldFallback: statusCode >= 500 || statusCode == 429,
-            shouldReRegister: statusCode == 401 || statusCode == 403
-        );
+        new(shouldFallback: statusCode >= 500 || statusCode == 429);
 }
