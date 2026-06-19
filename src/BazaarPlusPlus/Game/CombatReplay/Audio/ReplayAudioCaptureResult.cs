435dc550d81ac0da6b2ecab499adedbdd28fd33b
Xinyu YANG <cauyxy@gmail.com>
2026-06-19 18:46:44 +0800

    Move replay mux output decision into muxer

---
+++ src/BazaarPlusPlus/Game/CombatReplay/Audio/ReplayAudioCaptureResult.cs
@@ -0,0 +1,22 @@
+#nullable enable
+
+namespace BazaarPlusPlus.Game.CombatReplay.Audio;
+
+internal readonly struct ReplayAudioCaptureResult
+{
+    public string WavPath { get; init; }
+
+    public bool CapturedAnySamples { get; init; }
+
+    public long CapturedSampleFloats { get; init; }
+
+    public long FileSizeBytes { get; init; }
+
+    public string CapturePointLabel { get; init; }
+
+    public double RmsAmplitude { get; init; }
+
+    public float PeakAmplitude { get; init; }
+
+    public bool Usable { get; init; }
+}
